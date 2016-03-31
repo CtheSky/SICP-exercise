a01f1ff4ec933e540e833460c9516f65ad5ea9a3
CtheSky <cthesky@yeah.net>
2016-03-31 14:53:40 +0800

    Add--3-05.scm

---
+++ 3-05.scm
@@ -0,0 +1,24 @@
+(load "c:\\scheme\\ex\\3.1.2.scm")
+(load "c:\\scheme\\ex\\basic.scm")
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (rect x1 x2 y1 y2)
+  (abs (* (- x1 x2) (- y1 y2))))
+
+(define (estimate-integral p x1 x2 y1 y2 trials)
+  (let ((integral-test 
+	 (lambda ()
+	   (p (random-in-range x1 x2)
+	      (random-in-range y1 y2)))))
+    (* (rect x1 x2 y1 y2)
+       (monte-carlo trials integral-test))))
+
+(define (inside-unit-circle? x y)
+  (>= 1 
+      (+ (square x) (square y))))
+
+(define (estimate-pi)
+  (estimate-integral inside-unit-circle? -1.0 1.0 -1.0 1.0 10000))
