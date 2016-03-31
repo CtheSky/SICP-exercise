a78f349c9b51df1a11595352d76d8ca0d9885df7
CtheSky <cthesky@yeah.net>
2016-03-31 15:17:25 +0800

    Add--3-06.scm

---
+++ 3-06.scm
@@ -0,0 +1,15 @@
+(load "c:\\scheme\\ex\\3.1.2.scm")
+
+(define rand
+  (let ((x random-init))
+    (lambda (m)
+      (cond ((eq? m 'generate)
+	     (set! x (random-update x))
+	     x)
+	    ((eq? m 'reset)
+	     (lambda (set-value)
+	       (begin (set! x set-value)
+		      'set-value-done)))
+	    (else
+	     (error "Unknown request -- RAND"
+		    m))))))
