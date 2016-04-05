d6042b1be5b8791f8885f075c39806bd265f7e59
CtheSky <cthesky@yeah.net>
2016-04-05 10:23:42 +0800

    Add--3-24.scm

---
+++ 3-24.scm
@@ -0,0 +1,32 @@
+(define (make-table same-key?)
+  (define (assoc key records)
+    (cond ((null? records) #f)
+	  ((same-key? key (caar records)) (car records))
+	  (else (assoc key (cdr records)))))
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record (assoc key-2 (cdr subtable))))
+	      (if record
+		  (cdr record)
+		  #f))
+	    #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record (assoc key-2 (cdr subtable))))
+	      (if record
+		  (set-cdr! record value)
+		  (set-cdr! subtable
+			    (cons (cons key-2 value)
+				  (cdr subtable)))))
+	    (set-cdr! local-table
+		      (cons (list key-1 (cons key-2 value))
+			    (cdr local-table)))))
+      'done)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+	    ((eq? m 'insert!) insert!)
+	    (else (error "UNKOWN operation -- TABLE" m))))
+    dispatch))
