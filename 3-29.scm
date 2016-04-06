a9a14c99a51e1e6ed7683cdf10ec7c7dfd352b2c
CtheSky <cthesky@yeah.net>
2016-04-06 13:20:28 +0800

    Add--3-29.scm

---
+++ 3-29.scm
@@ -0,0 +1,13 @@
+(load "c:\\scheme\\ex\\3.3.4.scm")
+
+;;or-gate could implemented by and-gate & inverter
+;;a + b -> (a'b')'
+(define (or-gate a b output)
+  (let ((a- (make-wire))
+	(b- (make-wire))
+	(a-b- (make-wire)))
+    (inverter a a-)
+    (inverter b b-)
+    (and-gate a- b- a-b-)
+    (inverter a-b- output)
+  'ok))
