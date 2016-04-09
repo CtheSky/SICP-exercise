6399714d67501d39384ced4546b6863abdaaedd0
CtheSky <cthesky@yeah.net>
2016-04-09 14:36:14 +0800

    Add--3-37.scm

---
+++ 3-37.scm
@@ -0,0 +1,32 @@
+(load "c:\\scheme\\ex\\3.3.5.scm")
+
+(define (c+ a b)
+  (let ((sum (make-connector)))
+    (adder a b sum)
+    sum))
+
+(define (c* a b)
+  (let ((product (make-connector)))
+    (multiplier a b product)
+    product))
+
+(define (c/ a b)
+  (let ((quotient (make-connector)))
+    (divider a b quotient)
+    quotient))
+
+(define (cv val)
+  (let ((v (make-connector)))
+    (constant val v)
+    v))
+
+(define (c-f-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+	  x)
+      (cv 32)))
+
+(define c (make-connector))
+(define f (c-f-converter c))
+
+(probe "c temp: " c)
+(probe "f temp: " f)
