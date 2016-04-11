83517d82e42aed8bb2a38caf5a6b34e80a4133d0
CtheSky <cthesky@yeah.net>
2016-04-11 16:15:23 +0800

    Add--3-50.scm

---
+++ 3-50.scm
@@ -0,0 +1,9 @@
+(load "c:\\scheme\\ex\\3.5.1.scm")
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+	      (cons proc (map stream-cdr argstreams))))))
