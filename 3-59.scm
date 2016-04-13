87388204e37969a9a8717d5af578ec5770f8c55c
CtheSky <cthesky@yeah.net>
2016-04-13 14:41:46 +0800

    Add--3-59.scm

---
+++ 3-59.scm
@@ -0,0 +1,16 @@
+(load "c:\\scheme\\ex\\3.5.2.scm")
+
+(define (integrate-series argu-stream)
+  (mul-streams argu-stream
+	       (stream-map (lambda (x) (/ 1 x))
+			   integers)))
+
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+
+(define cosine-series
+  (cons-stream 1 (integrate-series (stream-map (lambda (x) (* -1 x)) 
+					       sine-series))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
