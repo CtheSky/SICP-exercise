4e5537c30acbb6b9381225a9664690baf7d144eb
CtheSky <cthesky@yeah.net>
2016-04-14 19:52:23 +0800

    Add--3-62.scm

---
+++ 3-62.scm
@@ -0,0 +1,10 @@
+(load "c:\\scheme\\ex\\3-61.scm")
+
+(define (div-series num denom)
+  (let ((denom-const (stream-car denom)))
+    (if (= 0 denom-const)
+	(error "Denom constant is zero -- DIV-SERIES")
+	(mul-series 
+	 (invert-after1-series
+	  (scale-stream denom denom-const))
+	 num))))
