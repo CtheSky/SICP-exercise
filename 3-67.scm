8acb225c8e7534971144619920f73604bcab1362
CtheSky <cthesky@yeah.net>
2016-04-16 11:59:48 +0800

    Add--3-67.scm

---
+++ 3-67.scm
@@ -0,0 +1,11 @@
+(load "c:\\scheme\\ex\\3.5.3.scm")
+
+(define (full-pairs s t)
+  (cons-stream (list (stream-car s) (stream-car t))
+	       (interleave (stream-map (lambda (x) (list (stream-car s) x))
+				       (stream-cdr t))
+			   (interleave (stream-map (lambda (x) (list x (stream-car t)))
+						   (stream-cdr s))
+				       (full-pairs (stream-cdr s)
+						   (stream-cdr t))))))
+					       
