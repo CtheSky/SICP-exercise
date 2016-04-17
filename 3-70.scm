b2cab7aa918e2eb4759cae349319adfb8ee4a683
CtheSky <cthesky@yeah.net>
2016-04-17 16:01:26 +0800

    Add--3-70.scm

---
+++ 3-70.scm
@@ -0,0 +1,22 @@
+(load "c:\\scheme\\ex\\3.5.3.scm")
+
+(define (weighted-pairs s t weight)
+  (cons-stream (list (stream-car s) (stream-car t))
+	       (merge-weighted (stream-map (lambda (x) (list (stream-car s) x))
+					   (stream-cdr t))
+			       (weighted-pairs (stream-cdr s)
+					       (stream-cdr t)
+					       weight)
+			       weight)))
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(else
+	 (let ((s1-car (stream-car s1))
+	       (s2-car (stream-car s2)))
+	   (if (< (weight s1-car)
+		  (weight s2-car))
+	       (cons-stream s1-car
+			    (merge-weighted (stream-cdr s1) s2 weight))
+	       (cons-stream s2-car 
+			    (merge-weighted s1 (stream-cdr s2) weight)))))))
