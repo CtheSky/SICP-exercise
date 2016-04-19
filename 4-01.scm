4532cf242d1c86393432a66eed58ea64bcd57ae0
CtheSky <cthesky@yeah.net>
2016-04-19 14:27:36 +0800

    Add--4-01.scm

---
+++ 4-01.scm
@@ -0,0 +1,16 @@
+;;left -> right version
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((left (eval (first-operand exps) env))
+	    (right (list-of-values (rest-operands exps env))))
+	(cons left right))))
+
+
+;;left <- right version
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-of-values (rest-operands exps env)))
+	    (left (eval (first-operand exps) env)))
+	(cons left right))))
