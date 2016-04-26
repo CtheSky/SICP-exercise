535630e86dbae3c0f778f9bb62278ec5db456979
CtheSky <cthesky@yeah.net>
2016-04-26 15:15:55 +0800

    Fix-- Change let --> let*
    Original one still depends on the evaluating-order of let

--- 4-01.scm
+++ 4-01.scm
@@ -2,8 +2,8 @@
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (let ((left (eval (first-operand exps) env))
-	    (right (list-of-values (rest-operands exps env))))
+      (let* ((left (eval (first-operand exps) env))
+	     (right (list-of-values (rest-operands exps env))))
 	(cons left right))))
 
 
@@ -11,6 +11,6 @@
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (let ((right (list-of-values (rest-operands exps env)))
-	    (left (eval (first-operand exps) env)))
+      (let* ((right (list-of-values (rest-operands exps env)))
+	     (left (eval (first-operand exps) env)))
 	(cons left right))))
